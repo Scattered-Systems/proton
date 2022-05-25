a6cb16e62e9ac51bcc409ed1f7efe4ee419e8f21
Joe McCain III <92560746+fl03@users.noreply.github.com>
2022-05-25 10:40:51 -0500

    Commit
    Signed-off-by: FL03 <jo3mccain@icloud.com>

---
+++ .space.kts
@@ -0,0 +1,14 @@
+job("Build and Push: Proton") {
+    docker {
+        build {
+            context = "./bin"
+            file = "./bin/Dockerfile"
+            args["HTTP_PROXY"] = "http://0.0.0.0:8080"
+            labels["vendor"] = "scattered-systems"
+        }
+
+        push("scattered-systems.registry.jetbrains.space/p/scsys/containers/proton") {
+            tags("0.0.\$JB_SPACE_EXECUTION_NUMBER", "latest")
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# proton
-A highly-customizable interface that is powered by the Scattered-Ecosystem and grants users full, applied control over their Ethereum Names.
+# chaos
+
+A complete decentralized cloud storage management solution
