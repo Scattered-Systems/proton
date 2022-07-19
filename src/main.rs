c1e9f606fa3067668f53aee17fd3c8744ec26d6d
FL03 <jo3mccain@icloud.com>
2022-07-19 09:46:22 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ pub use crate::{app::*, core::*};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Welcome to Proton");
 
-    let proton = Proton::new();
-    &proton.run();
+    let mut proton = Proton::new();
+    proton.run();
     Ok(())
 }
 
@@ -12,35 +12,37 @@ mod app {
     use crate::{TEXT_BOX_WIDTH, VERTICAL_WIDGET_SPACING, WINDOW_TITLE};
     use druid::{self, Widget, WidgetExt};
 
-    pub struct Face {
-        pub height: f64,
-        pub width: f64,
+    pub trait Gui {
+        fn constructor() -> dyn Widget<Store>;
     }
 
-    #[derive(Clone, druid::Data, druid::Lens)]
+    #[derive(Clone, Debug, druid::Data, druid::Lens)]
     pub struct Store {
+        ensname: String,
         name: String,
     }
 
     #[derive(Clone, Debug)]
     pub struct Proton {
-        pub name: String,
+        pub initial_state: Store,
     }
 
     impl Proton {
         pub fn new() -> Self {
             Self {
-                name: "Proton".to_string(),
+                initial_state: Store {
+                    ensname: "".to_string(),
+                    name: "".to_string(),
+                },
             }
         }
-
         /// Create the main window
         fn constructor() -> impl Widget<Store> {
             let label = druid::widget::Label::new(|data: &Store, _env: &druid::Env| {
                 format!("Hello {}!", data.name)
             });
 
-            let textbox = druid::widget::TextBox::new()
+            let input_name = druid::widget::TextBox::new()
                 .with_placeholder("Who are we greeting?")
                 .fix_width(TEXT_BOX_WIDTH)
                 .lens(Store::name);
@@ -48,22 +50,16 @@ mod app {
             let layout = druid::widget::Flex::column()
                 .with_child(label)
                 .with_spacer(VERTICAL_WIDGET_SPACING)
-                .with_child(textbox);
+                .with_child(input_name);
 
             druid::widget::Align::centered(layout)
         }
-
-        pub fn run(&self) {
+        pub fn run(&mut self) {
             let main_window = druid::WindowDesc::new(Self::constructor)
                 .title(WINDOW_TITLE)
-                .window_size((400.0, 400.0));
-
-            let initial_state = Store {
-                name: "World".into(),
-            };
-
+                .window_size((800.0, 800.0));
             druid::AppLauncher::with_window(main_window)
-                .launch(initial_state)
+                .launch(self.initial_state.clone())
                 .expect("Failed to launch application");
         }
     }
