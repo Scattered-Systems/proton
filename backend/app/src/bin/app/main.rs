68d7098363f77b2639ccff64731ea89f9d0e51ad
Joe McCain III <jo3mccain@scattered-systems.com>
2022-10-13 01:15:04 +0000

    Update

--- backend/app/src/bin/app/main.rs
+++ backend/app/src/bin/app/main.rs
@@ -12,9 +12,19 @@ mod application;
 #[tokio::main]
 async fn main() -> scsys::core::BoxResult {
     let app = Backend::new();
-    app.with_logging()
-        .run()
-        .await
-        .expect("Application startup failed...");
+    // app.with_logging().run().await?;
+    sample::docker_sample().await?;
     Ok(())
+}
+
+pub(crate) mod sample {
+    use bollard::Docker;
+    use futures::FutureExt;
+
+    pub async fn docker_sample() -> scsys::core::BoxResult {
+        let docker = Docker::connect_with_local_defaults()?;
+        let version = docker.version().await.unwrap();
+        println!("{:?}", version);
+        Ok(())
+    }
 }
