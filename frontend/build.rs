6db13321aa20d0fc7dc768f278c72bf9a86f6379
Joe McCain III <jo3mccain@icloud.com>
2022-10-24 21:37:03 +0000

    Update

--- frontend/build.rs
+++ frontend/build.rs
@@ -1,3 +1,13 @@
+use substrate_wasm_builder::WasmBuilder;
+
 fn main() {
-    println!("Building the crate...");
+    WasmBuilder::new()
+        // Tell the builder to build the project (crate) this `build.rs` is part of.
+        .with_current_project()
+        // Make sure to export the `heap_base` global, this is required by Substrate
+        .export_heap_base()
+        // Build the Wasm file so that it imports the memory (need to be provided by at instantiation)
+        .import_memory()
+        // Build it.
+        .build()
 }
--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -3,6 +3,7 @@
     Contrib: FL03 <jo3mccain@icloud.com>
     Description: ... summary ...
 */
+
 pub use self::core::*;
 
 pub mod actors;
