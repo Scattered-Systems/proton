9897ba9ffd8c03a00df2a4d51f924ea094af673f
Joe McCain III <jo3mccain@icloud.com>
2022-11-12 18:56:25 +0000

    Update

---
+++ proton/src/core/mod.rs
@@ -0,0 +1,25 @@
+/*
+    Appellation: core <module>
+    Contrib: FL03 <jo3mccain@icloud.com>
+    Description: ... summary ...
+*/
+pub use self::{primitives::*, utils::*};
+
+pub(crate) mod primitives {
+
+
+}
+
+pub(crate) mod utils {
+    use wasm_bindgen::prelude::*;
+
+    #[wasm_bindgen]
+    pub fn add_one(data: usize) -> usize {
+        data + 1
+    }
+
+    #[wasm_bindgen]
+    pub fn timestamp() -> i64 {
+        scsys::prelude::Timestamp::default().into()
+    }
+}
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,6 @@ npm install -g npm@8.19.2
 rustup default nightly
 rustup component add clippy rustfmt --toolchain nightly
 rustup target add wasm32-unknown-unknown --toolchain nightly
-cargo install create-tauri-app wasm-bindgen-cli
+cargo install create-tauri-app wasm-bindgen-cli wasm-pack
 cargo install --locked trunk
 sudo apt -y update && sudo apt -y upgrade && sudo apt -y autoremove
