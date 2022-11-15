073cb68cae65d1dc029e2947140174f9e3941d73
Joe McCain III <jo3mccain@icloud.com>
2022-11-15 06:01:29 +0000

    Update

--- app/frontend/src/lib/nav/navbar/Navbar.svelte
+++ app/frontend/src/lib/nav/navbar/Navbar.svelte
@@ -3,7 +3,7 @@
   import { info } from '$lib/constants';
 
   import List from '$lib/list/List.svelte';
-  import NavBanner from '$lib/nav/navbar/banner/NavBanner.svelte';
+  import Banner from '$lib/banner/Banner.svelte';
   import Wallet from '$lib/login/wallet/Wallet.svelte';
 
   export const links = [];
@@ -11,7 +11,10 @@
 </script>
 
 <nav class="absolute bg-transparent flex flex-nowrap items-center justify-between inset-x-0 mt-3 p-3 text-white top-0 w-full z-50">
-  <NavBanner label="{info.name}"/>
+  <div class="flex xl:hidden">
+    <Banner label="{info.name}"/>
+  </div>
+  
   <div class="lg:flex grow items-center justify-start sm:hidden xs:hidden" id="main-menu">
     <List props="flex flex-col lg:flex-row list-none mr-auto">
       {#each links as view}
--- app/frontend/src/lib/nav/sidebar/Sidebar.svelte
+++ app/frontend/src/lib/nav/sidebar/Sidebar.svelte
@@ -1,6 +1,9 @@
 <script>
     /** @type {import('./$types').PageData} */
     export let data;
+
+    import { info } from '$lib/constants.js';
+    import Banner from '$lib/banner/Banner.svelte';
 	import Text from '$lib/misc/text/Text.svelte';
     import { selectedAccount, web3 } from 'svelte-web3';
 
@@ -9,32 +12,23 @@
 </script>
 
 
-<div class="bg-zinc-800 hidden xl:flex items-center justify-between m-0 h-full py-3">
-    <div class="flex flex-col items-center justify-between h-full py-12">
-        <div class="divide-y">
-            <div class="flex flex-col flex-initial">
-                <span class="h2 text-center">Sidebar</span>
-            </div>
-            <div class="flex-col py-3">
-                
-                <div class="flex flex-col grow mb-auto">
-                    
-                    <Text>
-        
-                        {$selectedAccount}
-        
-                    </Text>
-                </div>
-                <div class="flex flex-col mt-auto">
-                    
-                    <Text>
-                        Controls
-        
-                    </Text>
-                </div>
+<div class="bg-zinc-800 hidden xl:flex items-center justify-center h-full w-full">
+    <div class="divide-y flex flex-col h-full">
+        <div class="hidden xl:flex p-3 mx-auto items-center justify-center">
+            <div class="flex p-3">
+                <Banner label="{info.name}"/>
             </div>
             
         </div>
+        <div class="flex flex-col justify-between py-3">
+            <div class="flex flex-col grow mb-auto">
+                <Text>
+                    {$selectedAccount}
+                </Text>
+            </div>
+            <div class="flex flex-col mt-auto">
+                <Text>Controls</Text>
+            </div>
+        </div>
     </div>
-    
 </div>
