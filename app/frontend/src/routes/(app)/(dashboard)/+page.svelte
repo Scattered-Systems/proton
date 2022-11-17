04974cfd54b7169d71c5473ee74830fae4b17bdf
Joe McCain III <jo3mccain@icloud.com>
2022-11-17 20:04:03 +0000

    Update

--- app/frontend/src/routes/(app)/(dashboard)/+page.svelte
+++ app/frontend/src/routes/(app)/(dashboard)/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+    import ProfileCard from '$lib/cards/profile/ProfileCard.svelte';
+
     /** @type {import('./$types').PageData} */
     export let data;
 	
@@ -9,11 +11,16 @@
 	<meta name="description" content="{data.head.description}" />
 </svelte:head>
 
-<section class="flex nowrap items-center justify-center py-24 h-full w-full">
-    <div class="flex bg-zinc-800 grow mx-auto rounded h-full w-full ">
-        <div class=" p-3 flex grow no-wrap items-center justify-center h-full w-full">
-            <span class="">Content Section</span>
-        </div>
+<div class="flex nowrap items-center justify-between py-24 h-full w-full">
+    <ProfileCard/>
+    <div class="flex grow mx-auto h-full w-full px-3">
+        <section class="bg-zinc-800 rounded h-full w-full">
+            <div class=" p-3 flex grow no-wrap items-center justify-center h-full w-full">
+            
+            
+            </div>
+
+        </section>
     </div>
     
-</section>
+</div>
