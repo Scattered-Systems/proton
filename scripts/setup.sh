e353e85f7ce7db341c25f1cef268dd5fcc1a2704
Joe McCain III <jo3mccain@icloud.com>
2022-11-19 17:33:21 +0000

    Update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-npm install -g npm@9.1.1
 rustup default nightly
 rustup component add clippy rustfmt --toolchain nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly
-sudo apt -y update && sudo apt -y upgrade && sudo apt -y autoremove
+rustup target add wasm32-wasi wasm32-unknown-unknown --toolchain nightly
+sudo apt -y update && sudo apt -y upgrade
 sudo apt install -y protobuf-compiler
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | bash
