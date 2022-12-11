c4611900e6ae1990ba73761479a8061ca3a6251e
Joe McCain III <jo3mccain@icloud.com>
2022-12-11 00:40:08 +0000

    Update

--- desktop/src/lib.rs
+++
@@ -1,6 +0,0 @@
-/*
-    Appellation: proton-desktop <binary>
-    Contrib: FL03 <jo3mccain@icloud.com>
-    description:
-        Proton is a unique runtime environment capable of engaging a myriad of providers
-*/
