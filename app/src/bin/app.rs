62d21fea1b279c1cde957e0fe824b54f33e30a0d
FL03 <jo3mccain@icloud.com>
2023-05-20 13:25:52 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- app/src/bin/app.rs
+++ app/src/bin/app.rs
@@ -2,18 +2,16 @@
     Appellation: app <binary>
     Contrib: FL03 <jo3mccain@icloud.com> (https://github.com/FL03)
 */
-extern crate dotenv;
 use proton_ui::{app, ApplicationScope};
 
 fn main() -> anyhow::Result<()> {
-    dotenv::dotenv().ok();
     let scope = ApplicationScope::new().with_name("Proton");
     starter(scope)
 }
 
 fn starter(scope: ApplicationScope) -> anyhow::Result<()> {
     #[cfg(target_family = "wasm")]
-    dioxus_web::launch_with_props(app, scope, dioxus_web::Config::new().hydrate(true));
+    dioxus_web::launch_with_props(app, scope, dioxus_web::Config::new());
     #[cfg(any(macos, unix, windows))]
     dioxus_desktop::launch_with_props(app, scope, dioxus_desktop::Config::new());
     Ok(())
